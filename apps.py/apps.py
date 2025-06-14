ef6bdc994f0f935a3a6fbad0504e666688998b9c
AngeTrevice12 <treviceyao@gmail.com>
2025-06-14 02:47:21 +0000

    Update apps.py

--- apps.py/apps.py
+++ apps.py/apps.py
@@ -1,8 +1,19 @@
-from flask import Flask
+import streamlit as st
+import pandas as pd
 
-app = Flask(__name__)
+st.set_page_config(
+    page_title="Real-Time Data Science Dashboard",
+    page_icon="✅",
+    layout="wide",
+   initial_sidebar_state="expanded")
 
+st.title(" Visualisation des incendies actifs en temps réel✅🔥🌳")
 
+MAP_KEY= "4ec1e3c89060b2d307d5763358818604"
+civ_url  = 'https://firms.modaps.eosdis.nasa.gov/api/country/csv/'  +  MAP_KEY  +  '/VIIRS_NOAA20_NRT/CIV/5'
+df_civ  = pd.read_csv(civ_url)
+dt_filter = st.selectbox("Select one date", pd.unique(df_civ["acq_date"]))
+dff = df_civ[df_civ["acq_date"]== dt_filter]
+st.map(dff)
 
-if __name__ == '__main__':
-    app.run()
+st.dataframe(dff)
